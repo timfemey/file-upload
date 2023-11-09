4b38ef8544ad9db4ab9a6c45fb3b5bf07b77d161
timfemey <isholaobafemi@gmail.com>
2023-11-09 14:57:59 +0100

    patch: added express adn hyper-express

--- src/controller/server.ts
+++ src/controller/server.ts
@@ -1,3 +1,20 @@
-function checkServerType() {
+import express from "express"
+const expressInit = express()
+import { Server } from "hyper-express";
+import HyperExpress from "hyper-express"
 
+function checkServerType(server: "express"): typeof expressInit;
+function checkServerType(server: "hyper-express"): typeof Server;
+
+function checkServerType(server: "hyper-express" | "express"): typeof expressInit | typeof Server {
+    const Express = server == "hyper-express" ? HyperExpress.Server : expressInit
+    return Express;
+
+}
+
+
+
+function app(perf: boolean) {
+
+    perf ? checkServerType("hyper-express") : checkServerType("express")
 }
--- src/index.ts
+++ src/index.ts
@@ -6,28 +6,29 @@ import { program } from "commander";
 
 const totalCPU = cpus().length;
 
+program
+    .option("-p, --perf", "Enable performance mode", false)
+    .option("-s, --max-size <size>", "Maximum file size allowed", parseInt)
+    .option(
+        "-n, --max-files <count>",
+        "Maximum number of files allowed (not greater than 20)",
+        parseInt
+    )
+
+program.parse(process.argv);
+
+if (process.argv.slice(2).length < 1) {
+
+    program.outputHelp();
+    process.exit(1);
+
+}
+
+const options = program.opts()
+
 if (cluster.isWorker == false) {
-    program
-        .option("-p, --perf", "Enable performance mode", false)
-        .option("-s, --max-size <size>", "Maximum file size allowed", parseInt)
-        .option(
-            "-n, --max-files <count>",
-            "Maximum number of files allowed (not greater than 20)",
-            parseInt
-        )
 
-    program.parse(process.argv);
 
-    if (process.argv.slice(2).length < 1) {
-
-        program.outputHelp();
-        process.exit(1);
-
-    }
-
-    const options = program.opts()
-
-    console.log(options, options.perf, options.maxSize, options.maxFiles)
 
     if (options.maxSize === undefined || options.maxFiles === undefined) {
         console.error('Error: Arguments not provided.');
@@ -55,15 +56,7 @@ if (cluster.isWorker == false) {
     }
 
 
-    console.log('Arguments received:');
 
-    console.log('Performance Mode:', options.perf);
-
-    console.log('Max File Size:', options.maxSize);
-
-    console.log('Max Number of Files:', options.maxFiles);
-    console.log(`Number of CPUs is ${totalCPU}`);
-    console.log("Attempting to Start Server")
 
     //Fork Workers
     for (let i = 0; i < totalCPU; i++) {
@@ -78,6 +71,16 @@ if (cluster.isWorker == false) {
         cluster.fork();
     });
 } else {
+
+    // console.log('Arguments received:');
+
+    // console.log('Performance Mode:', options.perf);
+
+    // console.log('Max File Size:', options.maxSize);
+
+    // console.log('Max Number of Files:', options.maxFiles);
+    // console.log(`Number of CPUs is ${totalCPU}`);
+    // console.log("Attempting to Start Server")
 
 
 
